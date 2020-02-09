77f9c18f081da8b312488f44fb659995d3faa520
Victor Sued <visued@gmail.com>
2020-02-09 17:55:38 -0300

    Http: Upload de Arquivo: Back-end com Node.js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'cursos'},
-  {path: 'cursos', loadChildren: './cursos/cursos.module#CursosModule'},
-  {path: 'rxjs-poc', loadChildren: './unsubscribe-rxjs/unsubscribe-rxjs.module#UnsubscribeRxjsModule'}
+  { path: '', pathMatch: 'full', redirectTo: 'upload-file' },
+  { path: 'cursos', loadChildren: './cursos/cursos.module#CursosModule' },
+  { path: 'rxjs-poc', loadChildren: './unsubscribe-rxjs/unsubscribe-rxjs.module#UnsubscribeRxjsModule' },
+  { path: 'upload-file', loadChildren: './upload-file/upload-file.module#UploadFileModule' }
 ];
 
 @NgModule({
---
+++ src/app/upload-file/upload-file.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrls: ['./upload-file.component.scss']
+})
+export class UploadFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onChange(event) {
+    const selectedFiles = <FileList>event.srcElement.files;
+    //document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
+    const fileNames = [];
+
+    for(let i = 0; i < selectedFiles.length; i++) {
+      fileNames.push(selectedFiles[i].name);
+    }
+    document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+  }
+
+}
