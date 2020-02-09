c05722564176acb932c9c969b3bb9a2573a4acc8
Victor Sued <visued@gmail.com>
2020-02-09 18:28:26 -0300

    Http Upload de Arquivo: Request com FormData

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -1,25 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UploadFileService } from "./upload-file.service";
 
 @Component({
-  selector: 'app-upload-file',
-  templateUrl: './upload-file.component.html',
-  styleUrls: ['./upload-file.component.scss']
+  selector: "app-upload-file",
+  templateUrl: "./upload-file.component.html",
+  styleUrls: ["./upload-file.component.scss"]
 })
 export class UploadFileComponent implements OnInit {
+  files: Set<File>;
 
-  constructor() { }
+  constructor(private uploadService: UploadFileService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   onChange(event) {
     const selectedFiles = <FileList>event.srcElement.files;
-    //document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
+    this.files = new Set();
     const fileNames = [];
 
-    for(let i = 0; i < selectedFiles.length; i++) {
+    for (let i = 0; i < selectedFiles.length; i++) {
       fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
     }
-    document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+    document.getElementById("customFileLabel").innerHTML = fileNames.join(", ");
   }
 
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.uploadService
+        .upload(this.files, "http://localhost:8000/uploads")
+        .subscribe(response => console.log("Upload concluido!"));
+    }
+  }
 }
