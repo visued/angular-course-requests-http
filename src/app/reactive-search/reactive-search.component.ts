1fa9aee4898fd3a3b6f22f78287937469aa94261
Victor Sued <visued@gmail.com>
2020-02-10 09:07:36 -0300

    Http: Pesquisa/Busca com Programação Reativa

--- src/app/reactive-search/reactive-search.component.ts
+++ src/app/reactive-search/reactive-search.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
-import { map, tap } from "rxjs/operators";
+import {
+  map,
+  tap,
+  filter,
+  distinctUntilChanged,
+  debounceTime,
+  switchMap
+} from "rxjs/operators";
 
 @Component({
   selector: "app-reactive-search",
@@ -14,30 +21,37 @@ export class ReactiveSearchComponent implements OnInit {
   SEARCH_URL = "https://api.cdnjs.com/libraries";
   results$: Observable<any>;
   total: number;
+  readonly FIELDS = "name,description,version,homepage";
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.results$ = this.queryField.valueChanges.pipe(
+      map(value => value.trim()),
+      filter(value => value.length > 1),
+      debounceTime(200),
+      distinctUntilChanged(),
+      switchMap(value =>
+        this.http.get(this.SEARCH_URL, {
+          params: {
+            search: value,
+            fields: this.FIELDS
+          }
+        })
+      ),
+      tap((res: any) => this.total = res.total),
+      map((res: any) => res.results)
+    );
+  }
 
   onSearch() {
     let value = this.queryField.value;
-
+    const fields = "name,description,version,homepage";
 
     if (value && (value = value.trim()) !== "") {
-      const fields = 'name,description,version,homepage';
-
-      const params = {
-        search: value,
-        fields: fields
-      }
       this.results$ = this.http
-        .get(
-          this.SEARCH_URL, { params}
-        )
-        .pipe(
-          tap((res: any) => (this.total = res.total)),
-          map((res: any) => res.results)
-        );
+        .get(this.SEARCH_URL, { params })
+        .pipe(map((res: any) => res.results));
     }
   }
 }
