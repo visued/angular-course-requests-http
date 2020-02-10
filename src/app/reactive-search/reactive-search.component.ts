588e333035e5f8a9596b60e32c7c0ea8ffe3bd07
Victor Sued <visued@gmail.com>
2020-02-10 08:42:33 -0300

    Http: Passando Parâmetros na URL (HttpParams)

--- src/app/reactive-search/reactive-search.component.ts
+++ src/app/reactive-search/reactive-search.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { map, tap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-reactive-search',
-  templateUrl: './reactive-search.component.html',
-  styleUrls: ['./reactive-search.component.scss']
+  selector: "app-reactive-search",
+  templateUrl: "./reactive-search.component.html",
+  styleUrls: ["./reactive-search.component.scss"]
 })
 export class ReactiveSearchComponent implements OnInit {
-
   queryField = new FormControl();
+  SEARCH_URL = "https://api.cdnjs.com/libraries";
+  results$: Observable<any>;
+  total: number;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSearch() {
+    let value = this.queryField.value;
+
+
+    if (value && (value = value.trim()) !== "") {
+      const fields = 'name,description,version,homepage';
+
+      const params = {
+        search: value,
+        fields: fields
+      }
+      this.results$ = this.http
+        .get(
+          this.SEARCH_URL, { params}
+        )
+        .pipe(
+          tap((res: any) => (this.total = res.total)),
+          map((res: any) => res.results)
+        );
+    }
   }
-
 }
